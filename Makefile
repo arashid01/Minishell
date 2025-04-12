8890f5416a47cb2bdef8c51d47a320b482e3d76c
Amal <a.rashid2000@outlook.com>
2025-04-12 09:17:57 +0300

    new, blank

---
+++ Makefile
@@ -0,0 +1,34 @@
+NAME = minishell
+CC = cc
+
+LIBFT_DIR = includes/libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+READLINE_DIR = /usr/local/Cellar/readline/8.2.13
+
+CFLAGS = -Wall -Werror -Wextra -I$(LIBFT_DIR) -I$(READLINE_DIR)/include
+LDFLAGS = -L$(LIBFT_DIR) -lft -L$(READLINE_DIR)/lib -lreadline
+
+SRCS =  srcs/main.c srcs/tokenizing.c srcs/parsing_1.c \
+		srcs/clean_utils.c srcs/parsing_2.c srcs/execute.c
+OBJS = $(SRCS:.c=.o)
+
+all: $(LIBFT) $(NAME)
+
+$(NAME): $(OBJS) 
+	$(CC) $(CFLAGS) $(OBJS) $(LDFLAGS) -o $(NAME)
+
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR) || exit 1
+
+clean:
+	rm -f $(OBJS)
+	$(MAKE) -C $(LIBFT_DIR) fclean
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amal <amal@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 01:31:38 by amal              #+#    #+#             */
+/*   Updated: 2025/04/12 09:06:55 by amal             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+
+#endif
