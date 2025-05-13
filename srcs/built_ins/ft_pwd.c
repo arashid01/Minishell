f35cf5db38e698c37698bdf1370ac4fbd5bc6c42
Amal <a.rashid2000@outlook.com>
2025-05-13 04:05:16 +0300

    fixing execution norm

--- srcs/built_ins/ft_pwd.c
+++ srcs/built_ins/ft_pwd.c
@@ -1,43 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amal <amal@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/30 21:21:17 by nora              #+#    #+#             */
-/*   Updated: 2025/05/13 03:47:18 by amal             ###   ########.fr       */
+/*   Created: 2025/04/30 23:58:52 by nora              #+#    #+#             */
+/*   Updated: 2025/05/13 03:17:02 by amal             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	ft_echo(char **argv)
-{
-	int	i;
-	int	j;
-	int	newline;
-
-	i = 1;
-	newline = 1;
-	while (argv[i] && !ft_strncmp(argv[i], "-n", 2))
-	{
-		j = 2;
-		while (argv[i][j] == 'n')
-			j++;
-		if (argv[i][j] != '\0')
-			break ;
-		newline = 0;
-		i++;
-	}
-	while (argv[i])
-	{
-		printf("%s", argv[i]);
-		if (argv[i + 1])
-			printf(" ");
-		i++;
-	}
-	if (newline)
-		printf("\n");
-	return (0);
-}
--- srcs/execution/execution.c
+++ srcs/execution/execution.c
@@ -6,18 +6,32 @@
 /*   By: amal <amal@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 15:23:25 by amal              #+#    #+#             */
-/*   Updated: 2025/05/12 00:22:17 by amal             ###   ########.fr       */
+/*   Updated: 2025/05/13 03:44:32 by amal             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	execute_command(t_cmd *cmd, char **envp, int in_fd, int out_fd)
+void	handle_builtin_cmd(t_cmd *cmd, int in_fd, int out_fd)
 {
-	int	fds[2];
-	pid_t	pid;
 	int		saved_stdin;
 	int		saved_stdout;
+	
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	handle_input_redirection(cmd, &in_fd);
+	handle_output_redirection(cmd, &out_fd);
+	execute_builtin(cmd);
+	dup2(saved_stdin, STDIN_FILENO);
+	dup2(saved_stdout, STDOUT_FILENO);
+	close(saved_stdin);
+	close(saved_stdout);
+}
+
+void	execute_command(t_cmd *cmd, char **envp, int in_fd, int out_fd)
+{
+	int		fds[2];
+	pid_t	pid;
 
 	if (!cmd)
 		return ;
@@ -26,15 +40,7 @@ void	execute_command(t_cmd *cmd, char **envp, int in_fd, int out_fd)
 
 	if (is_builtin_cmd(cmd) && !cmd->has_pipe)
 	{
-		saved_stdin = dup(STDIN_FILENO);
-		saved_stdout = dup(STDOUT_FILENO);
-		handle_input_redirection(cmd, &in_fd);
-		handle_output_redirection(cmd, &out_fd);
-		execute_builtin(cmd);
-		dup2(saved_stdin, STDIN_FILENO);
-		dup2(saved_stdout, STDOUT_FILENO);
-		close(saved_stdin);
-		close(saved_stdout);
+		handle_builtin_cmd(cmd, in_fd, out_fd);
 		return ;
 	}		
 	pid = fork();
